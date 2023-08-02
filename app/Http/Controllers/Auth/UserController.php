a75431e8ad7b76aa12e66fd40657f859071fe95a
Fatemeh Khoshgoftar <Fatima.Khoshgoftar@gmail.com>
2023-08-02 18:31:05 +0330

    make SignUpUserRequest and and  Sign_up function in UserController

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -3,22 +3,29 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\SignUpUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
-    public function login()
+    public function login(LoginUserRequest $request)
     {
-        \request()->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
-        $user = User::where('email', \request('email'))->first();
-        if (! $user || ! Hash::check(\request('password'), $user->password)) {
+        $request = $request->validated();
+        $user = User::where('email', $request['Email'])->first();
+        if (! $user || ! Hash::check($request['password'], $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
         }
         return $user->createToken('token_base_name')->plainTextToken;
+    }
+    public function sign_up(SignUpUserRequest $request){
+        $request = $request->validated();
+        $code = OTP::generate();
+        dd($code);
     }
 }
--- app/Http/Requests/SignUpUserRequest.php
+++ app/Http/Requests/SignUpUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class LoginUserRequest extends FormRequest
+class SignUpUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,6 +22,7 @@ class LoginUserRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'name'=> ['string','required'],
             'Email' => ['required','email'],
             'password' => ['required']
         ];
